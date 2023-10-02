fd585a1fca1d0bd9568acca10b9486e3e44d6da4
Mar4ik10 <markflint2608@gmail.com>
2023-10-02 21:16:18 +0200

    Add image to index.html, change settings.py(add theatre app, MEDIA_URL and MEDIA_ROOT to urls.py)

--- theatre/theatre/settings.py
+++ theatre/theatre/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'theatrehome',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +123,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'public'
 ]
 
+
+MEDIA_ROOT = BASE_DIR / 'public'
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
